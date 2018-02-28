c3875254793f326514952342e130261b32d0fffe
Eltzem <eltzem@uni.edu>
2018-02-28 10:44:34 -0600

    Changed function of csvreader

--- csvreader.py
+++ csvreader.py
@@ -1,30 +1,44 @@
+from array import array
+
 import timestep
-
-
+import csv
+import numpy as np
+from scipy import stats
 # Converts a cvs file to a list
 class csvreader():
 
-    def __init__(self, _filename):
-        source = open(_filename, "r")
-        self.aList = []
-        global aList;
-        i = 0
-        for line in source:
-            aline = line.split(",")
-            self.aList.append(timestep.timestep(aline))
-            i += 1
-        self.aList.append("\n")
+    def __init__(self,_filename):
+        filepathNew = _filename + "_data.csv"
+        with open(_filename) as fOld:
+            csvOld = csv.reader(fOld)
+            with open(filepathNew, 'w') as fNew:
+                csvNew = csv.writer(fNew)
 
-        temp = 'trendlines_' + _filename + '.txt'
-        target = open('temp', 'w')
+                next(csvOld)
+                pastFive = []
 
-    def str_print(self):
-        for item in self.aList:
-            print(str(self.aList[item]))
+                for row in csvOld:
 
-    def data_calculator(self, target, filename):
-        for line in target:
-            self.aList[i]
+                    # Approximates a readable queue for calculating the trend lines
+                    # If we don't have 5 items yet, we don't need to shift
+                    if(pastFive.__len__()>4):
+                        for i in range(4, 0, -1):
+                            pastFive[i] = pastFive[i-1]
+                        pastFive[0]=(row[7])
+                    else:
+                        pastFive.append((row[7]))
 
-# linear trendline 5 timesteps
-# ln trendline
+                    # From list of stings to np array of ints
+                    linearInput = np.array(list(map(float, pastFive)))
+
+                    # generating "time" for regression
+                    x = np.array(range(1,pastFive.__len__()+1))
+
+                    slope, intercept, r_value, p_value, std_err = stats.linregress(x, linearInput)
+
+                    row.append(slope)
+
+                    csvNew.writerow(row)
+
+        fOld.close()
+        fNew.close()
