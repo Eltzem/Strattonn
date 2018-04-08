5508b7346934a2a7e14ead5e617a07700c1ef4b4
William Spurr <spurrw@uni.edu>
2018-04-08 10:30:24 -0500

    modified gitignore and make CreateMasterDataFile.py work

--- CreateMasterDataFile.py
+++ CreateMasterDataFile.py
@@ -5,10 +5,11 @@ import os.path
 if __name__ == '__main__':
     masterFile = csv.writer(open('data/1min/MASTER.csv', 'w'))
 
-    for filename in os.listdir():
+    for filename in os.listdir('data/1min'):
         first = False
 
-        if 'csv' in filename and filename != 'MASTER.csv' and not '_' in filename:
+        if 'csv' in filename and not 'MASTER' in filename and not '_' in filename \
+                and not 'back' in filename and not '3910' in filename:
             fromFile = csv.reader(open('data/1min/' + filename))
         
             firstLine = next(fromFile)
